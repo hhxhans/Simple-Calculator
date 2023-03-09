a14b333480b6cb20176638e1ff8302a193b05d2d
hhxhans <213192693@seu.edu.cn>
2023-03-09 20:27:55 +0800

    Initial Commit

---
+++ Simple-Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple-Calculator
+//
+//  Created by niudan on 2023/3/9.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
